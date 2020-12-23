PurplePachyderm/tensorslow

commit 84855f3643ea128de035774ecfd3896772bd899d

Add Convolutional Neural Networks
A new ConvolutionalNetwork model type has been created. It reuses
convolution functions implemented in previous commits, and will be
tested on the CIFAR example (and probably in a dedicated test, similar
to the MLP one).

Output rescaling has also been removed from the ReLU function for
performance reasons.

include/model.hpp
@@ -17,6 +17,7 @@ namespace ts {
 
 	template <typename T> class Polynom;
 	template <typename T> class MultiLayerPerceptron;
+	template <typename T> class ConvolutionalNetwork;
 
 	// Friends forward declaration
 	template <typename T> class GradientAccumulator;
@@ -89,6 +90,37 @@ public:
 
 	ts::Tensor<T> (*activationFunction)(const ts::Tensor<T>&) = &(ts::sigmoid);
 
+	std::vector<ts::Tensor<T>> weights = {};
+	std::vector<ts::Tensor<T>> biases = {};
+
+	void toggleGlobalOptimize(bool enable);
+
+	ts::Tensor<T> compute(ts::Tensor<T> input);
+
+	void save(std::string filePath);
+	void load(std::string filePath);
+};
+
+
+
+	// ts::ConvolutionalNetwork
+
+template <typename T>
+class ts::ConvolutionalNetwork : public ts::Model<T> {
+private:
+
+public:
+	ConvolutionalNetwork(
+		std::vector<unsigned> inputSize,
+		std::vector<std::vector<unsigned>> convLayers,
+		std::vector<unsigned> denseLayers
+	);
+
+	ts::Tensor<T> (*activationFunction)(const ts::Tensor<T>&) = &(ts::sigmoid);
+
+
+	std::vector<unsigned> expectedInput;
+	std::vector<ts::Tensor<T>> convKernels = {};
 	std::vector<ts::Tensor<T>> weights = {};
 	std::vector<ts::Tensor<T>> biases = {};
 

src/model.cpp
@@ -155,17 +155,29 @@ ts::MultiLayerPerceptron<T>::MultiLayerPerceptron(
 	// Each element of the layers vector is a new layer, its value represents
 	// the layer size. Values are randomly initialized between 0 and 1.
 
+	// Make sure layers/input are not of size 0
+	if(inputSize == 0) {
+		return;
+	}
+
+	for(unsigned i=1; i<layers.size(); i++) {
+		if(layers[i] == 0) {
+			return;
+		}
+	}
+
+
 	layers.insert(layers.begin(), inputSize);
 
 	for(unsigned i=1; i<layers.size(); i++) {
-		// Add layer i-1 -> layer i weights (and make it optimizable)
+		// Add layer i-1 -> layer i weights
 		weights.push_back(ts::Tensor<T>(
 			Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic>()
 			.setRandom(layers[i], layers[i-1]),
 			&(this->wList))
 		);
 
-		// Add layer i biases (and make it optimizable)
+		// Add layer i biases
 		biases.push_back(ts::Tensor<T>(
 			Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic>()
 			.setRandom(layers[i], 1),
@@ -233,6 +245,182 @@ void ts::MultiLayerPerceptron<T>::load(std::string filePath) {
 	// Load new tensors
 	std::ifstream in(filePath);
 
+	weights = ts::parseTensorsVector(in, &(this->wList));
+	biases = ts::parseTensorsVector(in, &(this->wList));
+
+	in.close();
+}
+
+
+
+	// ts::ConvolutionalNetwork
+
+template <typename T>
+ts::ConvolutionalNetwork<T>::ConvolutionalNetwork(
+	std::vector<unsigned> inputSize,
+	std::vector<std::vector<unsigned>> convLayers,
+	std::vector<unsigned> denseLayers
+) {
+	// inputSize : std::vector of size 2 for dimensions of 2D image / matrix
+	// convLayers : sizes of convolution kernels (std::vector of dimension 2)
+	// fullLayers: sizes of fully connected layers
+
+
+		// Validate dimensions of network
+
+	if(inputSize.size() != 2) {
+		return;
+	}
+	if(inputSize[0] == 0 || inputSize[1] == 0) {
+		return;
+	}
+
+	std::vector<int> intermediarySize = {(int) inputSize[0], (int) inputSize[1]};
+
+	// Make sure convolutions are possible
+	for(unsigned i=0; i<convLayers.size(); i++) {
+		// Is size of kernel correctly described
+		if(convLayers[i].size() != 2) {
+			return;
+		}
+		if(convLayers[i][0] == 0 || convLayers[i][0] == 0) {
+			return;
+		}
+
+		// Compute size of matrix after convolution
+		intermediarySize[0] = intermediarySize[0] - convLayers[i][0] + 1;
+		intermediarySize[1] = intermediarySize[1] - convLayers[i][1] + 1;
+
+		if(intermediarySize[0] <= 0 || intermediarySize[1] <= 0) {
+			return;
+		}
+	}
+
+	// Make sure layers are not of size 0
+	for(unsigned i=0; i<denseLayers.size(); i++) {
+		if(denseLayers[i] == 0) {
+			return;
+		}
+	}
+
+
+		// Randomly init kernels, weights and biases
+
+	// Convolution layers
+	for(unsigned i=0; i<convLayers.size(); i++) {
+		convKernels.push_back(ts::Tensor<T>(
+			Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic>()
+			.setRandom(convLayers[i][0], convLayers[i][1]),
+			&(this->wList))
+		);
+	}
+
+	// Fully connected layers
+	denseLayers.insert(
+		// First dense layer input will be flattened convolution output
+		denseLayers.begin(), intermediarySize[0] * intermediarySize[1]
+	);
+
+	for(unsigned i=1; i<denseLayers.size(); i++) {
+		// Add layer i-1 -> layer i weights
+		weights.push_back(ts::Tensor<T>(
+			Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic>()
+			.setRandom(denseLayers[i], denseLayers[i-1]),
+			&(this->wList))
+		);
+
+		// Add layer i biases
+		biases.push_back(ts::Tensor<T>(
+			Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic>()
+			.setRandom(denseLayers[i], 1),
+			&(this->wList))
+		);
+	}
+
+}
+
+
+
+template <typename T>
+void ts::ConvolutionalNetwork<T>::toggleGlobalOptimize(bool enable) {
+	if(weights.size() != biases.size()) {
+		return;
+	}
+
+	for(unsigned i=0; i<convKernels.size(); i++) {
+		this->toggleOptimize(&(convKernels[i]), enable);
+	}
+
+	for(unsigned i=0; i<weights.size(); i++) {
+		this->toggleOptimize(&(weights[i]), enable);
+		this->toggleOptimize(&(biases[i]), enable);
+	}
+}
+
+
+
+template <typename T>
+ts::Tensor<T> ts::ConvolutionalNetwork<T>::compute(ts::Tensor<T> input) {
+
+	// NOTE It might be a good idea to add an entire function to make sure that
+	// all parameters are compatible (in terms of size), and that output is
+	// computable
+
+	// Make sure input is large enough for first convolution
+	if(
+	input.getValue().rows() != expectedInput[0] ||
+	input.getValue().cols() != expectedInput[1]
+	) {
+		return ts::Tensor<T>(
+			Eigen::Array<T, 0, 0>(),
+			&(this->wList)
+		);
+	}
+
+	// 1) Convolution computation loop
+	for(unsigned i=0; i<convKernels.size(); i++) {
+		input = (*activationFunction)(convolution(input, convKernels[i]));
+		input = maxPooling(input);
+	}
+
+	// 2) Flatten convolution output
+	input = flattening(input);
+
+	// 3) Dense layers computation loop
+	for(unsigned i=0; i<weights.size(); i++) {
+		input = (*activationFunction)(matProd(weights[i], input) + biases[i]);
+	}
+
+	return input;
+}
+
+
+
+template <typename T>
+void ts::ConvolutionalNetwork<T>::save(std::string filePath) {
+	std::ofstream out(filePath);
+
+	out << ts::serializeTensorsVector(convKernels);
+	out << ts::serializeTensorsVector(weights);
+	out << ts::serializeTensorsVector(biases);
+
+	out.close();
+}
+
+
+
+template <typename T>
+void ts::ConvolutionalNetwork<T>::load(std::string filePath) {
+	// Delete current tensors and reset wList
+	convKernels = {};
+	weights = {};
+	biases = {};
+	this->wList.reset();
+
+	// Load new tensors
+	std::ifstream in(filePath);
+
+	convKernels = ts::parseTensorsVector(in, &(this->wList));
 	weights = ts::parseTensorsVector(in, &(this->wList));
 	biases = ts::parseTensorsVector(in, &(this->wList));
 

examples/mnist.cpp
@@ -297,7 +297,7 @@ int main(void) {
 	// ts::GradientDescentOptimizer<float> optimizer(learningRate);
 
 	optimizer.epochs = nEpochs;
-	
+
 	std::cout << "Training model..." << std::endl;
 	std::vector<std::vector<std::vector< float >>> losses =
 	optimizer.run(model, trainingData);

src/autodiff_operations.cpp
@@ -197,30 +197,12 @@ ts::Tensor<T> ts::relu(const ts::Tensor<T> &x) {
 
 	// Apply cwise max function
 	Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic> res = x.value;
+	Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic> dx = x.value;
 
 	for(unsigned i=0; i<res.rows(); i++) {
-		for(unsigned j=0; j<res.rows(); j++) {
+		for(unsigned j=0; j<res.cols(); j++) {
 			res(i,j) =  (res(i,j) < 0) ? 0 : res(i,j);
-		}
-	}
-
-
-	// Rescale
-	T maxCoeff = res.maxCoeff();
-	if(maxCoeff != 0) {
-		res = res / maxCoeff;
-	}
-	else {
-		res.setZero();
-	}
-
-
-	// Get derivative
-	// (1 if coef != 0 ; 0 if coef == 0)
-	Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic> dx = res;
-	for(unsigned i=0; i<dx.rows(); i++) {
-		for(unsigned j=0; j<dx.rows(); j++) {
-			dx(i,j) =  (dx(i,j) != 0) ? 1.0 / maxCoeff : 0;
+			dx(i,j) =  (res(i,j) != 0) ? 1.0 : 0;
 		}
 	}
 

src/datatypes.cpp
@@ -51,6 +51,7 @@ template ts::Tensor<float> ts::squaredNorm(const ts::Tensor<float> &x);
 template class ts::Model<float>;
 template class ts::Polynom<float>;
 template class ts::MultiLayerPerceptron<float>;
+template class ts::ConvolutionalNetwork<float>;
 
 template class ts::TrainingData<float>;
 template class ts::GaElement<float>;
@@ -117,6 +118,7 @@ template ts::Tensor<double> ts::squaredNorm(const ts::Tensor<double> &x);
 template class ts::Model<double>;
 template class ts::Polynom<double>;
 template class ts::MultiLayerPerceptron<double>;
+template class ts::ConvolutionalNetwork<double>;
 
 template class ts::TrainingData<double>;
 template class ts::GaElement<double>;

tests/autodiff.cpp
@@ -245,15 +245,15 @@ TEST(AutodiffTest, Relu) {
 	// Expected result and derivative
 	Eigen::Array<float, 3, 3> expectedRes;
 	expectedRes <<
-	0, 0.976, 0,
-	0.286, 0, 0,
-	0, 1, 0.167;
+	0, 41, 0,
+	12, 0, 0,
+	0, 42, 7;
 
 	Eigen::Array<float, 3, 3> expectedDx;
 	expectedDx <<
-	0, 1.0f/42.0f, 0,
-	1.0f/42.0f, 0, 0,
-	0, 1.0f/42.0f, 1.0f/42.0f;
+	0, 1.0f, 0,
+	1.0f, 0, 0,
+	0, 1.0f, 1.0f;
 
 
 	// Get derivative

commit 54d5b365b782c47fa60cb84be10d8939cb17d1d0

Correct 84855f3 by adding pool size
Accidentally pushed code that caused a compilation error in 84855f3.
Pool size was missing in the maxPooling call. Issue should be resolved,
with pool size stored in ts::ConvolutionalNetwork.

include/model.hpp
@@ -113,6 +113,7 @@ public:
 	ConvolutionalNetwork(
 		std::vector<unsigned> inputSize,
 		std::vector<std::vector<unsigned>> convLayers,
+		std::vector<unsigned> poolingSize,
 		std::vector<unsigned> denseLayers
 	);
 
@@ -121,6 +122,7 @@ public:
 
 	std::vector<unsigned> expectedInput;
 	std::vector<ts::Tensor<T>> convKernels = {};
+	std::vector<unsigned> pooling;
 	std::vector<ts::Tensor<T>> weights = {};
 	std::vector<ts::Tensor<T>> biases = {};
 

src/model.cpp
@@ -259,6 +259,7 @@ template <typename T>
 ts::ConvolutionalNetwork<T>::ConvolutionalNetwork(
 	std::vector<unsigned> inputSize,
 	std::vector<std::vector<unsigned>> convLayers,
+	std::vector<unsigned> poolingSize,
 	std::vector<unsigned> denseLayers
 ) {
 	// inputSize : std::vector of size 2 for dimensions of 2D image / matrix
@@ -274,6 +275,7 @@ ts::ConvolutionalNetwork<T>::ConvolutionalNetwork(
 	if(inputSize[0] == 0 || inputSize[1] == 0) {
 		return;
 	}
+
 
 	std::vector<int> intermediarySize = {(int) inputSize[0], (int) inputSize[1]};
 
@@ -301,6 +303,15 @@ ts::ConvolutionalNetwork<T>::ConvolutionalNetwork(
 		if(denseLayers[i] == 0) {
 			return;
 		}
+	}
+
+
+		// Make sure pooling is possible
+	if(
+		poolingSize[0] % intermediarySize[0] != 0 ||
+		poolingSize[1] % intermediarySize[1] != 0
+	) {
+		return;
 	}
 
 
@@ -336,6 +347,10 @@ ts::ConvolutionalNetwork<T>::ConvolutionalNetwork(
 			&(this->wList))
 		);
 	}
+
+	// Set up data fields
+	expectedInput = inputSize;
+	pooling = poolingSize;
 
 }
 
@@ -380,7 +395,7 @@ ts::Tensor<T> ts::ConvolutionalNetwork<T>::compute(ts::Tensor<T> input) {
 	// 1) Convolution computation loop
 	for(unsigned i=0; i<convKernels.size(); i++) {
 		input = (*activationFunction)(convolution(input, convKernels[i]));
-		input = maxPooling(input);
+		input = maxPooling(input, pooling);
 	}
 
 	// 2) Flatten convolution output